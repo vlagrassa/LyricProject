ca38530c2a91451573bc662bf44e0db94df414a2
Vince <vincent.lagrassa@gmail.com>
2018-12-23 19:51:57 -0500

    Fix basic setters and getters

--- src/LyricLine.java
+++ src/LyricLine.java
@@ -7,10 +7,32 @@ public class LyricLine {
         return plaintexts.get(key);
     }
 
-    // Get the full list
-    public Map<String,String> getPlainTexts() {
+    public String setPlainText(String key, String val) {
+        return plaintexts.set(key, val);
+    }
+
+    // Get the full plaintext map
+    public Map<String,String> getPlainTextMap() {
         return plaintexts;
     }
 
+    public Map<String,String> setPlainTextMap(Map<String,String> newmap) {
+        Map<String,String> temp = plaintexts;
+        plaintexts = newmap;
+        return temp;
+    }
+
+    // Get the list of Lyric slices
+    public List<LyricSlice> getSlices() {
+        return slices;
+    }
+
+    public List<LyricSlice> setSlices(List<LyricSlice> newlist) {
+        List<LyricSlice> temp = slices;
+        slices = newlist;
+        return temp;
+    }
+
+    // Order the list of Lyric slices based on category
     public void orderSlices();
 }
