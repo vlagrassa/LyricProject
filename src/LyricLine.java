0a1ad09c00bd55c5c6c4326117f4690026c470c8
Vince <vincent.lagrassa@gmail.com>
2018-12-26 18:17:59 -0500

    Add imports for HashMap and ArrayList

--- src/LyricLine.java
+++ src/LyricLine.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class LyricLine {
 
 // =-=-= Instance Variables =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
--- src/LyricSlice.java
+++ src/LyricSlice.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class LyricSlice /*implements Comparable<LyricSlice>*/ {
 
 // =-=-= Instance Variables =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
