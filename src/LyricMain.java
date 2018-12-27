434a03fb081e5b9ce02bd878c5049a4e80283a1f
Vince <vincent.lagrassa@gmail.com>
2018-12-26 19:18:09 -0500

    Test slice filtering with getSlices(x)

--- src/LyricMain.java
+++ src/LyricMain.java
@@ -2,17 +2,25 @@ public class LyricMain {
   
   public static void main(String[] args)
   {
-    String[] languages = {"English", "Japanese", "Kanji"};
+    String[] languages = {"English", "Japanese"};
     LyricLine testLine = new LyricLine(languages);
     testLine.setPlainText("Japanese", "honbun tesuto");
     testLine.setPlainText("English", "test text");
     System.out.println(testLine.getPlainText("Japanese"));
     System.out.println(testLine.getPlainText("English"));
 
-    LyricSlice testSlice = testLine.createSlice();
+    LyricSlice testSlice = testLine.createSlice(1);
     testSlice.setStartEnd("Japanese", -5, 6);
     testSlice.setStartEnd("English", 5, 18);
     System.out.println("Test Slice:\n" + testSlice);
     System.out.println("Test Line:\n" + testLine);
+
+    testLine.createSlice(1).setStartEnd("English", 7, 8);
+    testLine.createSlice(2).setStartEnd("English", 0, 4);
+
+    System.out.println("\nNo filter:");
+    System.out.println(testLine.getSlices());
+    System.out.println("\nFiltered by category 1:");
+    System.out.println(testLine.getSlices(1));
   }
 }
