24f43c8385fc123ae1e6eb63751f224fbd504c01
Vince <vincent.lagrassa@gmail.com>
2018-12-26 19:10:21 -0500

    Update testLine to initialize with languages

--- src/LyricMain.java
+++ src/LyricMain.java
@@ -2,12 +2,13 @@ public class LyricMain {
   
   public static void main(String[] args)
   {
-    LyricLine testLine = new LyricLine();
+    String[] languages = {"English", "Japanese", "Kanji"};
+    LyricLine testLine = new LyricLine(languages);
     testLine.setPlainText("Japanese", "honbun tesuto");
     testLine.setPlainText("English", "test text");
-    System.out.println(testLine.getPlainText("Romaji"));
+    System.out.println(testLine.getPlainText("Japanese"));
     System.out.println(testLine.getPlainText("English"));
-    
+
     LyricSlice testSlice = testLine.createSlice();
     testSlice.setStartEnd("Japanese", -5, 6);
     testSlice.setStartEnd("English", 5, 18);
