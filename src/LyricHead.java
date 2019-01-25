2bfc63cd259ae729cfbdc8759246ffd42a0b6cbd
Vince <vincent.lagrassa@gmail.com>
2019-01-24 21:55:44 -0600

    Methods to add, remove, move, and check for language

--- src/LyricHead.java
+++ src/LyricHead.java
@@ -22,13 +22,74 @@ public class LyricHead {
         return listOfLanguages;
     }
 
-    //TODO: Adding, removing, reordering languages
+    public String getLanguage(int index) {
+        return listOfLanguages.get(index);
+    }
+
+    public void setLanguageList(ArrayList<String> newLanguageList) {
+        listOfLanguages = newLanguageList;
+    }
+
+    public void addLanguage(String language) {
+        listOfLanguages.add(language);
+    }
+
+    public void addLanguage(int index, String language) {
+        listOfLanguages.add(index, language);
+    }
+
+    public void addLanguages(String... languages) {
+        for (String language : languages) {
+            addLanguage(language);
+        }
+    }
+
+    public void removeLanguage(String language) {
+        listOfLanguages.remove(language);
+    }
+
+    public void removeLanguage(int index) {
+        listOfLanguages.remove(index);
+    }
+
+    public void removeLanguages(String... languages) {
+        for (String language : languages) {
+            removeLanguage(language);
+        }
+    }
+
+    public Boolean moveLanguage(int currentIndex, int newIndex) {
+        String languageToMove = getLanguage(currentIndex);
+        removeLanguage(currentIndex);
+        if (newIndex < 0) {
+            addLanguage(0, languageToMove);
+            return false;
+        }
+        else if (newIndex > listOfLanguages.size()) {
+            addLanguage(languageToMove);
+            return false;
+        }
+        addLanguage(newIndex, languageToMove);
+        return true;
+    }
+
+    public Boolean moveLanguage(String language, int newIndex) {
+        return moveLanguage(listOfLanguages.indexOf(language), newIndex);
+    }
+
+    public Boolean hasLanguage(String language) {
+        return listOfLanguages.contains(language);
+    }
 
 
   // =-=-= Category List =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
     public ArrayList<LyricCategory> getCategories() {
         return listOfCategories;
+    }
+
+    public LyricCategory getCategory(int index) {
+        return listOfCategories.get(index);
     }
 
     public void setCategoryList(ArrayList<LyricCategory> newCategoryList) {
